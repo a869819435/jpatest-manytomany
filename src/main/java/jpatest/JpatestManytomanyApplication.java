d4a5c2f54b2b6aa32365d45864148bc8644f21fc
a869819435 <869819435@qq.com>
2020-12-10 09:33:59 +0800

    feat: import project

---
+++ src/main/java/jpatest/JpatestManytomanyApplication.java
@@ -0,0 +1,13 @@
+package jpatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpatestManytomanyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpatestManytomanyApplication.class, args);
+    }
+
+}
